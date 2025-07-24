24c73a41a78063e38a48fe371e4e0dabafeff0f6
tungndse <tungndse@gmail.com>
2025-07-24 13:02:38 +0700

    Some new type of errors for error handling

--- BackEnd/park_management/src/main/java/park/management/com/vn/exception/ticket/TicketException.java
+++ BackEnd/park_management/src/main/java/park/management/com/vn/exception/ticket/TicketException.java
@@ -1,4 +1,11 @@
 package park.management.com.vn.exception.ticket;
 
-public class TicketException {
+import park.management.com.vn.exception.BaseException;
+import park.management.com.vn.exception.ErrorCode;
+
+public abstract class TicketException extends BaseException {
+  public TicketException(String message, ErrorCode errorCode) {
+    super(message, errorCode);
+  }
+
 }
---
+++ BackEnd/park_management/src/main/java/park/management/com/vn/exception/ticket/TicketStatusInvalidException.java
@@ -0,0 +1,10 @@
+package park.management.com.vn.exception.ticket;
+
+import park.management.com.vn.exception.BaseException;
+import park.management.com.vn.exception.ErrorCode;
+
+public class TicketStatusInvalidException extends TicketException {
+    public TicketStatusInvalidException(String message) {
+        super(message, ErrorCode.TICKET_STATUS_NOT_VALID);
+    }
+}
