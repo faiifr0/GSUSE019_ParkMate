e47b8ad9fd903047abdccbb4d3f0c54c1f9c9088
loint5201 <113002077+loint5201@users.noreply.github.com>
2025-08-04 20:09:48 +0700

    Update Notification.java

--- BackEnd/park_management/src/main/java/park/management/com/vn/entity/Notification.java
+++ BackEnd/park_management/src/main/java/park/management/com/vn/entity/Notification.java
@@ -1,15 +1,14 @@
 package park.management.com.vn.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import park.management.com.vn.constaint.NotificationStatus;
+import park.management.com.vn.constaint.NotificationType;
 import park.management.com.vn.entity.base.BaseEntity;
 
 @Entity
@@ -27,12 +26,14 @@ public class Notification extends BaseEntity {
   @Column(name = "message", nullable = false)
   private String message;
 
+  @Enumerated(EnumType.STRING)
   @Column(name = "notification_type")
-  private String notificationType; // 'email' or 'app'
+  private NotificationType notificationType; // 'email' or 'app'
 
   @Column(name = "sent_at")
   private LocalDateTime sentAt;
 
+  @Enumerated(EnumType.STRING)
   @Column(name = "status")
-  private String status; // 'sent', 'pending', 'failed'
+  private NotificationStatus status; // 'sent', 'pending', 'failed'
 }
