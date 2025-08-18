e511c21258b1d854f7770b93d6f6a85fd6275b75
faiifr0 <hoanghapho123@gmail.com>
2025-08-18 22:37:44 +0700

    2 Boilerplate Pages for pages that use table and calender & User Profile Page

--- FrontEnd_Desktop/parkmate-web-app/src/components/user-profile/UserInfoCard.tsx
+++ FrontEnd_Desktop/parkmate-web-app/src/components/user-profile/UserInfoCard.tsx
@@ -114,20 +114,20 @@ export default function UserInfoCard() {
                     <Label>Facebook</Label>
                     <Input
                       type="text"
-                      defaultValue="https://www.facebook.com/PimjoHQ"
+                      defaultValue=""
                     />
                   </div>
 
                   <div>
                     <Label>X.com</Label>
-                    <Input type="text" defaultValue="https://x.com/PimjoHQ" />
+                    <Input type="text" defaultValue="" />
                   </div>
 
                   <div>
                     <Label>Linkedin</Label>
                     <Input
                       type="text"
-                      defaultValue="https://www.linkedin.com/company/pimjo"
+                      defaultValue=""
                     />
                   </div>
 
@@ -135,7 +135,7 @@ export default function UserInfoCard() {
                     <Label>Instagram</Label>
                     <Input
                       type="text"
-                      defaultValue="https://instagram.com/PimjoHQ"
+                      defaultValue=""
                     />
                   </div>
                 </div>
