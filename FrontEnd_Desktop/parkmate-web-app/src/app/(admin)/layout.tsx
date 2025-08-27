721e69fd99aa873fb30c96ca32777c0081f04290
Nhat Minh Dang <dangminhnhat10988@gmail.com>
2025-08-27 10:56:03 +0700

    feat(admin-layout): add login check with cookie-based auth

--- FrontEnd_Desktop/parkmate-web-app/src/app/(admin)/layout.tsx
+++ FrontEnd_Desktop/parkmate-web-app/src/app/(admin)/layout.tsx
@@ -1,39 +1,19 @@
-"use client";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import ClientAdminLayout from "@/components/layouts/ClientAdminLayout";
 
-import { useSidebar } from "@/context/SidebarContext";
-import AppHeader from "@/layout/AppHeader";
-import AppSidebar from "@/layout/AppSidebar";
-import Backdrop from "@/layout/Backdrop";
-import React from "react";
-
-export default function AdminLayout({
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const { isExpanded, isHovered, isMobileOpen } = useSidebar();
+  // cookies() trả về Promise nên cần await
+  const cookieStore = await cookies();
+  const token = cookieStore.get("token")?.value;
 
-  // Dynamic class for main content margin based on sidebar state
-  const mainContentMargin = isMobileOpen
-    ? "ml-0"
-    : isExpanded || isHovered
-    ? "lg:ml-[290px]"
-    : "lg:ml-[90px]";
+  if (!token) {
+    redirect("/signin");
+  }
 
-  return (
-    <div className="min-h-screen xl:flex">
-      {/* Sidebar and Backdrop */}
-      <AppSidebar />
-      <Backdrop />
-      {/* Main Content Area */}
-      <div
-        className={`flex-1 transition-all  duration-300 ease-in-out ${mainContentMargin}`}
-      >
-        {/* Header */}
-        <AppHeader />
-        {/* Page Content */}
-        <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">{children}</div>
-      </div>
-    </div>
-  );
+  return <ClientAdminLayout>{children}</ClientAdminLayout>;
 }
