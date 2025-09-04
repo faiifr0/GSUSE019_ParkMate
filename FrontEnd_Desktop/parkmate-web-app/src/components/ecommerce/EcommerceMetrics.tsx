0f81c14c56f376940a29b65cd25def2427360058
faiifr0 <hoanghapho123@gmail.com>
2025-09-04 20:09:18 +0700

    Fit all the metric items on the same line on dashboard page

--- FrontEnd_Desktop/parkmate-web-app/src/components/ecommerce/EcommerceMetrics.tsx
+++ FrontEnd_Desktop/parkmate-web-app/src/components/ecommerce/EcommerceMetrics.tsx
@@ -5,7 +5,7 @@ import { ArrowDownIcon, ArrowUpIcon, BoxIconLine, GroupIcon } from "@/icons";
 
 export const EcommerceMetrics = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-6">
+    <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 md:gap-6">
       {/* <!-- Metric Item Start --> */}
       <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
         <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
