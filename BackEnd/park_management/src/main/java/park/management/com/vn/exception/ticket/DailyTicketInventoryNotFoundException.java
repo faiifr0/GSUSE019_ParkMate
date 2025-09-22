90445fe803b3f24cbb0eb60075393f458514e523
huyhqq <huynhquanghuy0903@gmail.com>
2025-09-22 23:58:42 +0700

    exception
    exception

--- BackEnd/park_management/src/main/java/park/management/com/vn/exception/ticket/DailyTicketInventoryNotFoundException.java
+++ BackEnd/park_management/src/main/java/park/management/com/vn/exception/ticket/DailyTicketInventoryNotFoundException.java
@@ -1,13 +1,11 @@
 package park.management.com.vn.exception.ticket;
 
+import java.time.LocalDate;
 import park.management.com.vn.exception.ErrorCode;
 
-import java.time.LocalDate;
-
 public class DailyTicketInventoryNotFoundException extends TicketException {
-    public DailyTicketInventoryNotFoundException(Long ticketTypeId, LocalDate ticketDate) {
-        super("Daily Inventory not found for Ticket Type with id: " + ticketTypeId
-                        + " and Date: " + ticketDate,
-                ErrorCode.TICKET_INVENTORY_NOT_FOUND);
+    public DailyTicketInventoryNotFoundException(Long ticketTypeId, LocalDate date) {
+        super(ErrorCode.TICKET_INVENTORY_NOT_FOUND,
+              "No inventory found for ticketType=" + ticketTypeId + " at date " + date);
     }
 }
--- BackEnd/park_management/src/main/java/park/management/com/vn/exception/ticket/TicketTypeNotFoundException.java
+++ BackEnd/park_management/src/main/java/park/management/com/vn/exception/ticket/TicketTypeNotFoundException.java
@@ -1,9 +1,9 @@
 package park.management.com.vn.exception.ticket;
+
 import park.management.com.vn.exception.ErrorCode;
 
-
 public class TicketTypeNotFoundException extends TicketException {
-    public TicketTypeNotFoundException(Long id) {
-        super("Ticket not found with id " + id, ErrorCode.TICKET_TYPE_NOT_FOUND);
+    public TicketTypeNotFoundException(Long ticketTypeId) {
+        super(ErrorCode.TICKET_TYPE_NOT_FOUND, "Ticket type not found: id=" + ticketTypeId);
     }
 }
