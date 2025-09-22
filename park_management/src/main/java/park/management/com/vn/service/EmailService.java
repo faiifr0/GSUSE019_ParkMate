d0711a558377df906517bd912b1f8d33f88b3ba5
loint5201 <113002077+loint5201@users.noreply.github.com>
2025-09-22 23:47:43 +0700

    Delete

--- park_management/src/main/java/park/management/com/vn/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package park.management.com.vn.service;
-
-public interface EmailService {
-    void sendHtml(String to, String subject, String htmlBody);
-    void sendText(String to, String subject, String textBody);
-}
